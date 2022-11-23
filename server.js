9169d14a10a093f7052b0042824525cdd147b137
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2022-11-23 14:46:34 +0530

    make it better

--- server.js
+++ server.js
@@ -48,6 +48,11 @@ app.post('/order', (req, res) => {
     client.placeConditionalOrder(parms).then((d) => {
         console.log(d);
         res.send(d)
+
+            (async () => {
+
+                axios.post('https://soulfox-bot.herokuapp.com/sendlog', d)
+            })()
     })
 })
 
@@ -60,4 +65,15 @@ app.post('/walletbalance', (req, res) => {
         .catch(err => {
             res.send(err)
         });
+})
+
+app.post('/sendlog', (req, res) => {
+
+    const data = req.body
+    const msg = `\n\n            NEW TRADE OPENED\n\nSIMBOL : ${data.symbol}\n\nTYPE : ${data.side}\n\n\QTY : ${data.qty}\n\nENTRY PRICE : ${data.price}\n\nTAKE PROFIT : ${data.take_Profit}\n\n\STOP LOSE : ${data.stop_loss}\n\nID : ${data.order_id}\n\n`
+        (async () => {
+
+            axios.get(`https://api.telegram.org/bot5129025740:AAF_asgA7Kbvxq-o3lqopFp7OfywA8KW8uU/sendMessage?chat_id=-1001592447140&text=${encodeURIComponent(msg)}`)
+        })()
+    res.send('ok')
 })
