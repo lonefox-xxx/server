5263d30658f7f187ff453b660703515ee3784565
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2022-12-20 08:00:10 +0530

    all fine

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.post('/sendlog', (req, res) => {
     const msg = `SIMBOL : ${data.symbol}\n\nTYPE : ${data.side}\n\n\QTY : ${data.qty}\n\nENTRY PRICE : ${data.price}\n\nTAKE PROFIT : ${data.take_profit}\n\n\STOP LOSE : ${data.stop_loss}\n\nID : ${data.order_id}\n\n`;
     (async () => {
         await axios.get(`https://api.telegram.org/bot5129025740:AAF_asgA7Kbvxq-o3lqopFp7OfywA8KW8uU/sendMessage?chat_id=-1001592447140&text=${encodeURIComponent(msg)}`);
-        await axios.post("https://soulfox-bot-db.herokuapp.com/putdata", data)
+        // await axios.post("https://soulfox-bot-db.herokuapp.com/putdata", data)
     })()
     res.send('ok')
 })
