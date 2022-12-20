df2d983f006e9727fc48444a44ed14b4e4f91e82
lonefox <96380261+lonefox-xxx@users.noreply.github.com>
2022-12-20 08:02:15 +0530

    all fine

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post('/order', (req, res) => {
                 console.log(result);
                 res.send(result);
                 (async () => {
-                    await axios.post('https://soulfox-bot.herokuapp.com/sendlog', result.result)
+                    await axios.post('https://web-production-3c8f.up.railway.app/sendlog', result.result)
                 })()
             })
         })
